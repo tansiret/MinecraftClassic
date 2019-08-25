b957eafd7edf5fc52032856bac021bf6e8a186a7
Lucy Erickson <superhuman741@gmail.com>
2019-08-24 17:49:28 -0700

    Update displaylevel.js

--- js/displaylevel.js
+++ js/displaylevel.js
@@ -2,8 +2,3 @@ function displayLevel() {
   var levelId = document.getElementById("levelId");
   document.getElementById("minecraft").src = "https://classic.minecraft.net/?join=" + levelId.value;
 }
-function getLevelId() {
-  var levelUrl = document.getElementById("minecraft").src;
-  var cutId = levelUrl.slice(36);
-  document.getElementById("receivedLvlId").innerHTML = cutId;
-}
