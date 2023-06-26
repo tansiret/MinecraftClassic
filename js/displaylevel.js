04192a4be0ac50e80a99af842143c6620cfb408b
CrobyCheese <118228252+CrobyCheese@users.noreply.github.com>
2023-06-26 14:10:40 -0700

    Update displaylevel.js

--- js/displaylevel.js
+++ js/displaylevel.js
@@ -1,4 +1,4 @@
 function displayLevel() {
   var levelId = document.getElementById("levelId");
-  document.getElementById("minecraft").src = "https://classic.minecraft.net/?join=" + levelId.value;
+  document.getElementById("minecraft").src = "https://suite.saltyspamz.xyz/games/games/mc-classic-multiplayer/?join=" + levelId.value;
 }
